09066a266f15d6c06fff60335946142eac603538
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2025-01-30 10:46:01 +0800

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 const API_TOKEN = process.env.API_TOKEN;
@@ -43,13 +51,43 @@ export default function Home() {
     <div>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      <ul>
-        {popularMoviesData.length > 0
-          ? popularMoviesData.map((movie: { id: number; title: string }) => (
-              <li key={movie.id}>{movie.title}</li>
-            ))
-          : !loading && <p>No movies found.</p>}
-      </ul>
+      <Carousel className="w-full">
+        <CarouselContent className="h-[600px]">
+          {popularMoviesData.length > 0 ? (
+            popularMoviesData.map(
+              (movie: {
+                id: number;
+                title: string;
+                poster_path: string;
+                backdrop_path: string;
+                overview: string;
+              }) => (
+                <CarouselItem key={movie.id} className="h-full">
+                  <div className="h-full">
+                    <Card className="h-full">
+                      <CardContent className="flex items-center justify-center h-full">
+                        {movie.backdrop_path ? (
+                          <img
+                            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                            alt={movie.title}
+                            className="w-full object-cover"
+                          />
+                        ) : (
+                          <div>No image available</div>
+                        )}
+                      </CardContent>
+                      <CarouselNext />
+                      <CarouselPrevious />
+                    </Card>
+                  </div>
+                </CarouselItem>
+              )
+            )
+          ) : !loading ? (
+            <p>No movies found.</p>
+          ) : null}
+        </CarouselContent>
+      </Carousel>
     </div>
   );
 }
