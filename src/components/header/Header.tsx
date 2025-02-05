6e61be2d64a625fe3c97ceb954edc34c17849e28
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2025-02-05 11:38:13 +0800

    header fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -75,77 +75,34 @@ export function Header() {
   };
 
   return (
-    <header className="fixed top-0 inset-x-0 z-20 h-[59px] bg-background flex items-center justify-between mx-auto max-w-[1280px] px-5">
-      <div className="flex gap-2 text-indigo-700">
-        <Film />
-        <button
-          className="font-inter text-xl italic font-bold leading-5 tracking-[0.32px]"
-          onClick={handleHomeClick}
-        >
-          MovieZ
-        </button>
-      </div>
-
-      <div className="hidden md:flex md:gap-3">
-        <DropdownMenu>
-          <DropdownMenuTrigger className="flex w-[97px] h-[36px] p-2.5 px-4 justify-center items-center gap-2 rounded-md border hover:bg-slate-100 duration-75 ease-in-out">
-            <ChevronDown />
-            Genre
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent className="w-[580px] h-[300] p-5">
-            <h1 className="font-bold text-2xl">Genres</h1>
-            <h2 className="text-base">See list of movies by genre</h2>
-            <DropdownMenuSeparator className="mt-3 mb-3" />
-
-            {genreList.map((genre) => (
-              <Badge
-                key={genre.id}
-                variant={'outline'}
-                className="mr-4 mb-4 cursor-pointer"
-                onClick={() => handleGenreSelect(genre.id.toString())} // Trigger genre selection
-              >
-                {genre.name}
-                <ChevronRight className="stroke-1" />
-              </Badge>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-        <Input placeholder="Search" className="h-[36px] w-[379px]" />
-      </div>
-
-      <div
-        className={`absolute top-0 left-0 flex justify-between items-center h-[59px] px-5 bg-background w-full transition-all duration-300 ease-in-out ${
-          isSearchVisible
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0 translate-y-[-100px]'
-        } md:hidden`}
-      >
-        <div className="absolute top-3 left-20">
-          <Search className="absolute top-2 left-2 w-[18px] h-[18px]" />
-          <Input
-            placeholder="Search"
-            className="h-[36px] pl-9"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          />
+    <header className="fixed top-0 inset-x-0 z-20 h-[59px] bg-background flex items-center justify-between mx-auto px-5 w-full">
+      <div className="max-w-[1280px] fixed top-0 inset-x-0 z-20 h-[59px] bg-background flex items-center justify-between mx-auto px-5">
+        <div className="flex gap-2 text-indigo-700">
+          <Film />
+          <button
+            className="font-inter text-xl italic font-bold leading-5 tracking-[0.32px]"
+            onClick={handleHomeClick}
+          >
+            MovieZ
+          </button>
         </div>
-        <div>
+
+        <div className="hidden md:flex md:gap-3">
           <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="flex justify-center items-center border rounded w-9 h-9">
-                <ChevronDown className="text-white" />
-              </div>
+            <DropdownMenuTrigger className="flex w-[97px] h-[36px] p-2.5 px-4 justify-center items-center gap-2 rounded-md border hover:bg-slate-100 duration-75 ease-in-out">
+              <ChevronDown />
+              Genre
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-[330px] p-5 ml-5">
+
+            <DropdownMenuContent className="w-[580px] h-[300] p-5">
               <h1 className="font-bold text-2xl">Genres</h1>
               <h2 className="text-base">See list of movies by genre</h2>
               <DropdownMenuSeparator className="mt-3 mb-3" />
+
               {genreList.map((genre) => (
                 <Badge
                   key={genre.id}
-                  variant={'secondary'}
+                  variant={'outline'}
                   className="mr-4 mb-4 cursor-pointer"
                   onClick={() => handleGenreSelect(genre.id.toString())} // Trigger genre selection
                 >
@@ -155,33 +112,78 @@ export function Header() {
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
-        <Button variant="outline" onClick={toggleSearch}>
-          <X />
-        </Button>
-      </div>
 
-      <div className="flex gap-4 items-center">
-        <Button
-          variant="outline"
-          className="w-9 h-9 md:hidden"
-          onClick={toggleSearch}
+          <Input placeholder="Search" className="h-[36px] w-[379px]" />
+        </div>
+
+        <div
+          className={`absolute top-0 left-0 flex justify-between items-center h-[59px] px-5 bg-background w-full transition-all duration-300 ease-in-out ${
+            isSearchVisible
+              ? 'opacity-100 translate-y-0'
+              : 'opacity-0 translate-y-[-100px]'
+          } md:hidden`}
         >
-          <Search />
-        </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setTheme(isDarkMode ? 'light' : 'dark')}
-          aria-label="Toggle theme"
-          className="w-9 h-9"
-        >
-          {isDarkMode ? (
-            <Sun className="h-5 w-5" />
-          ) : (
-            <Moon className="h-5 w-5" />
-          )}
-        </Button>
+          <div className="absolute top-3 left-20">
+            <Search className="absolute top-2 left-2 w-[18px] h-[18px]" />
+            <Input
+              placeholder="Search"
+              className="h-[36px] pl-9"
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div className="flex justify-center items-center border rounded w-9 h-9">
+                  <ChevronDown className="text-white" />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[330px] p-5 ml-5">
+                <h1 className="font-bold text-2xl">Genres</h1>
+                <h2 className="text-base">See list of movies by genre</h2>
+                <DropdownMenuSeparator className="mt-3 mb-3" />
+                {genreList.map((genre) => (
+                  <Badge
+                    key={genre.id}
+                    variant={'secondary'}
+                    className="mr-4 mb-4 cursor-pointer"
+                    onClick={() => handleGenreSelect(genre.id.toString())} // Trigger genre selection
+                  >
+                    {genre.name}
+                    <ChevronRight className="stroke-1" />
+                  </Badge>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <Button variant="outline" onClick={toggleSearch}>
+            <X />
+          </Button>
+        </div>
+
+        <div className="flex gap-4 items-center">
+          <Button
+            variant="outline"
+            className="w-9 h-9 md:hidden"
+            onClick={toggleSearch}
+          >
+            <Search />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => setTheme(isDarkMode ? 'light' : 'dark')}
+            aria-label="Toggle theme"
+            className="w-9 h-9"
+          >
+            {isDarkMode ? (
+              <Sun className="h-5 w-5" />
+            ) : (
+              <Moon className="h-5 w-5" />
+            )}
+          </Button>
+        </div>
       </div>
     </header>
   );
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -12,14 +12,17 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Movie } from '@/types/movie-type';
 import { Star } from 'lucide-react';
 import { Button } from '../ui/button';
+import { useRouter } from 'next/navigation';
 
 const Slider: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string>('');
   const [nowPlayingMoviesData, setNowPlayingMoviesData] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
 
   const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
   const API_TOKEN = process.env.API_TOKEN;
+  const router = useRouter();
 
   const getMovieData = async () => {
     try {
@@ -33,15 +36,52 @@ const Slider: React.FC = () => {
         }
       );
       setNowPlayingMoviesData(nowPlayingResponse.data.results);
-      setLoading(false);
     } catch (err) {
-      setLoading(false);
+      setError('Error fetching data');
       if (axios.isAxiosError(err)) {
-        setError(err.response?.data.status_message || 'Error fetching data');
-      } else {
-        setError('An unexpected error occurred.');
+        setError(err.response?.data.status_message || 'API Error');
       }
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const getTrailer = async (movieId: number) => {
+    try {
+      const trailerResponse = await axios.get(
+        `${TMDB_BASE_URL}/movie/${movieId}/videos?language=en-US`,
+        {
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+          },
+        }
+      );
+      const trailers = trailerResponse.data.results;
+      if (trailers.length > 0) {
+        const youtubeTrailer = trailers.find(
+          (trailer: any) => trailer.site === 'YouTube'
+        );
+        if (youtubeTrailer) {
+          setTrailerUrl(`https://www.youtube.com/embed/${youtubeTrailer.key}`);
+        } else {
+          setTrailerUrl(null);
+        }
+      }
+    } catch (err) {
+      setError('Error fetching trailer');
+    }
+  };
+
+  const handleMovieClick = (movieId: number) => {
+    router.push(`/detail/${movieId}`);
+  };
+
+  const handleWatchTrailer = (movieId: number) => {
+    getTrailer(movieId);
+  };
+
+  const closeTrailerModal = () => {
+    setTrailerUrl(null);
   };
 
   useEffect(() => {
@@ -56,14 +96,15 @@ const Slider: React.FC = () => {
             {nowPlayingMoviesData.slice(0, 10).map((movie) => (
               <CarouselItem key={movie.id}>
                 <Card>
-                  <CardContent className="flex  justify-center p-0">
-                    <div className="">
+                  <CardContent className="flex justify-center p-0">
+                    <div>
                       <Image
                         src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                         width={1000}
                         height={250}
-                        alt="Picture of the author"
-                        className="h-[247px] object-cover"
+                        alt={movie.title}
+                        onClick={() => handleMovieClick(movie.id)}
+                        className="h-[247px] object-cover cursor-pointer"
                       />
                       <div className="p-5">
                         <div className="flex justify-between">
@@ -83,7 +124,9 @@ const Slider: React.FC = () => {
                             {movie.overview}
                           </p>
                         </div>
-                        <Button>Watch trailer</Button>
+                        <Button onClick={() => handleWatchTrailer(movie.id)}>
+                          Watch trailer
+                        </Button>
                       </div>
                     </div>
                   </CardContent>
@@ -94,47 +137,67 @@ const Slider: React.FC = () => {
         </Carousel>
       </div>
 
+      {trailerUrl && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="relative w-full max-w-[800px]">
+            <button
+              className="absolute top-2 right-2 text-white text-2xl"
+              onClick={closeTrailerModal}
+            >
+              X
+            </button>
+            <iframe
+              width="100%"
+              height="450"
+              src={trailerUrl}
+              title="Movie Trailer"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      )}
+
       <div className="hidden lg:block">
-        <Carousel className="w-screens max-h-[600px] flex flex-col p-0 ">
+        <Carousel className="w-screen max-h-[600px] flex flex-col p-0 ">
           <CarouselContent>
             {nowPlayingMoviesData.slice(0, 10).map((movie) => (
               <CarouselItem key={movie.id}>
                 <div>
                   <Card>
-                    <CardContent className="p-0 h-[700px]">
-                      <div className="">
-                        <Image
-                          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                          width={2500}
-                          height={750}
-                          alt="Picture of the movie"
-                          className="absolute object-cover h-[700px]"
-                        />
+                    <CardContent className="p-0 h-[700px] relative">
+                      <Image
+                        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                        width={2500}
+                        height={750}
+                        alt={movie.title}
+                        onClick={() => handleMovieClick(movie.id)}
+                        className="absolute object-cover h-[700px] w-full"
+                      />
 
-                        <div className="p-5 relative top-0 left-0 min-h-[500px] w-screen flex flex-col justify-center ml-40 gap-4">
-                          <div className="gap-4">
-                            <h1 className="text-base font-bold">
-                              Now Playing:
-                            </h1>
-                            <h2 className="text-2xl font-bold tracking-tight truncate">
-                              {movie.title}
-                            </h2>
+                      <div className="p-5 relative top-0 left-0 min-h-[500px] w-full flex flex-col justify-center ml-40 gap-4 max-w-[300px]">
+                        <div className="gap-4">
+                          <h1 className="text-base font-bold">Now Playing:</h1>
+                          <h2 className="text-2xl font-bold tracking-tight truncate">
+                            {movie.title}
+                          </h2>
 
-                            <h2 className="text-base font-bold flex">
-                              <Star className="text-yellow-400" />
-                              {movie.vote_average} /10
-                            </h2>
-                          </div>
-                          <p className="text-sm font-normal line-clamp-5 w-[302px]">
-                            {movie.overview}
-                          </p>
-                          <Button
-                            variant="secondary"
-                            className="gap-2 h-10 w-[140px]"
-                          >
-                            Watch Trailer
-                          </Button>
+                          <h2 className="text-base font-bold flex">
+                            <Star className="text-yellow-400" />
+                            {movie.vote_average} /10
+                          </h2>
                         </div>
+                        <p className="text-sm font-normal line-clamp-5 w-[302px]">
+                          {movie.overview}
+                        </p>
+                        <Button
+                          variant="secondary"
+                          className="gap-2 h-10 w-[140px]"
+                          onClick={() => handleWatchTrailer(movie.id)}
+                        >
+                          Watch Trailer
+                        </Button>
                       </div>
                     </CardContent>
                   </Card>
@@ -146,6 +209,11 @@ const Slider: React.FC = () => {
           <CarouselNext className="absolute right-10 top-1/2 transform -translate-y-1/2 z-100" />
         </Carousel>
       </div>
+      {error && (
+        <div className="text-red-500 text-center mt-4">
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 };
